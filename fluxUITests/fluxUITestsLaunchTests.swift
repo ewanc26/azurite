cfeecff53a66045a88237d243eccd980ba59e665
Ewan Croft <git@ewancroft.uk>
2026-02-28 17:32:01 +0000

    Initial Commit

---
+++ fluxUITests/fluxUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  fluxUITestsLaunchTests.swift
+//  fluxUITests
+//
+//  Created by Ewan Croft on 28/02/2026.
+//
+
+import XCTest
+
+final class fluxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
