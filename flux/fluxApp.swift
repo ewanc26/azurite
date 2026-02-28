a10954360acc0625ae1716082e003e0b9d872035
Ewan Croft <git@ewancroft.uk>
2026-02-28 17:32:01 +0000

    Initial Commit

---
+++ flux/fluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  fluxApp.swift
+//  flux
+//
+//  Created by Ewan Croft on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct fluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
